0cde6f53566dd7705181c0285fbb7ba36637f859
danielmarbach <daniel.marbach@openplace.net>
2012-06-21 15:40:44 +0200

    Removed caliburn viewmodel bind convention and solved it with ninject (cherry picked from commit 940c6ac1fd6019bbaa5157229c7d2bcdd7ef44bf)

---
+++ source/StockTicker.Test/Actions/UseActionsActivationStrategyTest.cs
@@ -0,0 +1,63 @@
+﻿//-------------------------------------------------------------------------------
+// <copyright file="UseActionsActivationStrategyTest.cs" company="bbv Software Services AG">
+//   Copyright (c) 2012
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+namespace StockTicker.Actions
+{
+    using System;
+
+    using FluentAssertions;
+
+    using Moq;
+
+    using Ninject;
+    using Ninject.Activation.Strategies;
+
+    using Xunit;
+
+    public class UseActionsActivationStrategyTest
+    {
+        private readonly StandardKernel kernel;
+
+        public UseActionsActivationStrategyTest()
+        {
+            this.kernel = new StandardKernel();
+            this.kernel.Bind<TestObjectWithUseActions>().ToSelf();
+            this.kernel.Bind<IActionBuilder>().ToMethod(ctx => Mock.Of<IActionBuilder>());
+
+            this.kernel.Components.Add<IActivationStrategy, UseActionsActivationStrategy>();
+        }
+
+        private interface ITestObjectWithUseAction : IUseActions
+        {
+        }
+
+        [Fact]
+        public void WhenActivatedObjectUsesActions_ShouldAttachActionBuilder()
+        {
+            var useActions = this.kernel.Get<TestObjectWithUseActions>();
+
+            useActions.Actions.Should().NotBeNull();
+            useActions.Actions().Should().NotBeNull();
+        }
+
+        private class TestObjectWithUseActions : ITestObjectWithUseAction
+        {
+            public Func<IActionBuilder> Actions { internal get; set; }
+        }
+    }
+}
